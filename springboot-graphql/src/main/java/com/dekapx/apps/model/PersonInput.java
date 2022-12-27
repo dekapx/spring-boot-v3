60e4f5853841763426d4d23bb30cf3f1e0edfadf
dekapx <xkapil@gmail.com>
2022-12-27 22:57:44 +0000

    spring boot graphql

--- springboot-graphql/src/main/java/com/dekapx/apps/model/PersonInput.java
+++ springboot-graphql/src/main/java/com/dekapx/apps/model/PersonInput.java
@@ -2,11 +2,6 @@ package com.dekapx.apps.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Data
 public class PersonInput {
     private String firstName;
