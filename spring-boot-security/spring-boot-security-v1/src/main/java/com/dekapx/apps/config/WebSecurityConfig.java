b5dba0827dc357b9eaf3b64aab9fe0cd022d567f
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-08-21 22:59:58 +0100

    code clean up

---
+++ spring-boot-security/spring-boot-security-v1/src/main/java/com/dekapx/apps/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.dekapx.apps.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable()).authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager users() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password("{noop}p@ssw0rd")
+                        .authorities("read")
+                        .build()
+        );
+    }
+}
