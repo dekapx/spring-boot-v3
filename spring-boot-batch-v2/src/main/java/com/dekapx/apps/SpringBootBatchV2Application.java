2ec51b2a03bae206cf467d62f0edf31a5f107d26
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro.local>
2026-05-05 12:17:46 +0100

    code cleanup and refactoring

---
+++ spring-boot-batch-v2/src/main/java/com/dekapx/apps/SpringBootBatchV2Application.java
@@ -0,0 +1,13 @@
+package com.dekapx.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchV2Application.class, args);
+	}
+
+}
